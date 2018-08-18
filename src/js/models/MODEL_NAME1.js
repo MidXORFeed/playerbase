8a0530325306c38be28c193021134f284c7e4e3f
Wilton Ngo <wiltonngo@gmail.com>
2018-08-18 03:57:59 -0400

    - Added axios dev dependency

--- src/js/models/MODEL_NAME1.js
+++ src/js/models/MODEL_NAME1.js
@@ -1,5 +1,24 @@
-export default class MODEL_NAME1 {
-    constructor() {
-        
+import { key } from '../config'
+import { axios } from 'axios'
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+}
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults() {
+        try {
+            const res = axios(`https://api.steampowered.com/${interface}/${method}/v${version}/`);
+            this.result = res.data.recipes;
+            // console.log(this.result);
+        } catch (error) {
+            alert(error);
+        }
     }
 }
