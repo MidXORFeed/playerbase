ef3ca842b6f17f9f72ee3d64cc680f410d3918e7
Wilton Ngo <wiltonngo@gmail.com>
2018-08-29 18:47:38 -0400

    - Search model now changed into a list data structure - Now properly handles multiple search records

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -5,32 +5,45 @@ import axios from 'axios'
 export default class Search {
     constructor(steamid) {
         this.steamid = steamid;
+        this.searches = [];
     }
 
-    async getPlayerSummaries() {
-        const interfaceName = 'ISteamUser';
-        const methodName = 'GetPlayerSummaries';
-        const versionName = '2';
-        try {
-            const res = await axios.get(`https://api.steampowered.com/${interfaceName}/${methodName}/v${versionName}?key=${SteamAPIKey}&steamids=${this.steamid}`);
-        } catch(error) { 
-            console.log(error);
-        };
+    addSearch(steamid, search) {
+        if (!this.searches[steamid])
+            this.searches[steamid] = search;
     }
 
-    async getOwnedGames() {
+    isSearched(steamid) {
+        return this.searches.findIndex(el => el === steamid) !== -1;
+    }
+    
+    async getOwnedGames(steamid) {
         const interfaceName = 'IPlayerService';
         const methodName = 'GetOwnedGames';
         const versionName = '1';
         let include_appinfo = 1;
         let include_played_free_games = 1;
         let appids_filter = '';
+        this.steamid = steamid;
         try {
-            const res = await axios.get(`https://api.steampowered.com/${interfaceName}/${methodName}/v${versionName}/?key=${SteamAPIKey}&format=json&steamid=${this.steamid}&include_appinfo=${include_appinfo}&include_played_free_games=${include_played_free_games}&appids_filter=${appids_filter}`);
-            this.ownedGames = res.data.response.games;
-            return this.ownedGames;
+            if (!this.searches[steamid]) {
+                const res = await axios.get(`https://api.steampowered.com/${interfaceName}/${methodName}/v${versionName}/?key=${SteamAPIKey}&format=json&steamid=${steamid}&include_appinfo=${include_appinfo}&include_played_free_games=${include_played_free_games}&appids_filter=${appids_filter}`);
+                return res.data.response.games;
+            }
+            return this.searches[steamid];
         } catch (error) {
             console.log(error);
         }
     }
+
+    // async getPlayerSummaries() {
+    //     const interfaceName = 'ISteamUser';
+    //     const methodName = 'GetPlayerSummaries';
+    //     const versionName = '2';
+    //     try {
+    //         const res = await axios.get(`https://api.steampowered.com/${interfaceName}/${methodName}/v${versionName}?key=${SteamAPIKey}&steamids=${this.steamid}`);
+    //     } catch(error) { 
+    //         console.log(error);
+    //     };
+    // }
 }
