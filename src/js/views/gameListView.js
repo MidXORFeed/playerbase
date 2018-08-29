0dcd31abe8b8f185ac1239d44901953831461f23
Wilton Ngo <wiltonngo@gmail.com>
2018-08-29 18:51:13 -0400

    - gameListView will now display message if no games with community steam market are found from API call

--- src/js/views/gameListView.js
+++ src/js/views/gameListView.js
@@ -1,18 +1,26 @@
 import { elements, steamMarketApps } from '../playerbase'
 
+export const clearGamesList = () => {
+    elements.gameList.innerHTML = '';
+};
+
 export const renderGameList = (listOfGames) => {
     let markup = ``;
-    listOfGames.forEach(element => {
-        if (element.img_icon_url != "" && 
-            element.playtime_forever >= 0 && 
-            steamMarketApps.indexOf(element.appid) >= 0) {
-            const img_url = `http://media.steampowered.com/steamcommunity/public/images/apps/${element.appid}/${element.img_icon_url}.jpg`;
-            markup += 
-            `<li id=${element.appid} class="game_listItem">
-                <img src="${img_url}"></img>
-                <span>${element.name}</span>
-            </li>`
-        }
-    });
+    if (listOfGames) {
+        listOfGames.forEach(element => {
+            if (element.img_icon_url != "" && 
+                element.playtime_forever >= 0 && 
+                steamMarketApps.indexOf(element.appid) >= 0) {
+                const img_url = `http://media.steampowered.com/steamcommunity/public/images/apps/${element.appid}/${element.img_icon_url}.jpg`;
+                markup += 
+                `<li id=${element.appid} class="game_listItem">
+                    <img src="${img_url}"></img>
+                    <span>${element.name}</span>
+                </li>`
+            }
+        });
+    } else {
+        markup = 'Cannot find games with steam community market'
+    }
     elements.gameList.insertAdjacentHTML('afterbegin', markup);
 };
