dc38291951f539c45d620d7785b992270f8cf86d
Wilton Ngo <wiltonngo@gmail.com>
2018-08-29 18:49:31 -0400

    - Inventory model now uses a list data model

--- src/js/models/Inventory.js
+++ src/js/models/Inventory.js
@@ -3,19 +3,28 @@ import axios from 'axios'
 
 export default class Inventory {
     constructor() {
+        this.inventories = {};
+    }
 
+    addInventory(steamid, gameAppID, inventory) {
+        if (!this.inventories[steamid + '_' + gameAppID])
+            this.inventories[steamid + '_' + gameAppID] = inventory;
+    }
+
+    isRetrieved(steamid, gameAppID) {
+        return this.inventories[steamid + '_' + gameAppID] !== undefined;
     }
 
     async getInventoryData(steamid, appid) {
-        try {
-            if (!this.inventoryData) {
+        if (!this.inventories[steamid + '_' + appid]) {
+            try {
                 const res = await axios.get(`${proxy}http://steamcommunity.com/inventory/${steamid}/${appid}/2?l=english&count=5000`);
-                this.inventoryData = res.data.descriptions;
+                return res.data.descriptions;
+            } catch (error) {
+                console.log(error);
+                console.log("Failed to retrieve inventory data for game");
             }
-            return this.inventoryData;
-        } catch (error) {
-            console.log(error);
-            console.log("Failed to retrieve inventory data for game");
         }
+        return this.inventories[steamid + '_' + appid];
     }
 }
