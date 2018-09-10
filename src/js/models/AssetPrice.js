a2706d4b97e27c222a064250d76173ce9af1d196
Wilton Ngo <wiltonngo@gmail.com>
2018-09-10 07:03:35 -0400

    - Setup object to hold recentItemSalesInfo - Setup getRecentSalesInfo async method

--- src/js/models/AssetPrice.js
+++ src/js/models/AssetPrice.js
@@ -7,6 +7,7 @@ export default class AssetPrice {
     constructor() {
         this.allItemPrices = {};
         this.priceDataForItemsOnSale = {};
+        this.recentItemSalesInfo = {};
     }
 
     addAllItemPrices(gameAppID, assetPrices) {
@@ -34,12 +35,24 @@ export default class AssetPrice {
         }
     }
 
+    addRecentSalesInfo(gameAppID, marketHashName, recentItemSalesInfo) {
+        if (!this.recentItemSalesInfo[gameAppID]) { this.recentItemSalesInfo[gameAppID] = {} };
+        if (!this.recentItemSalesInfo[gameAppID][marketHashName]) {
+            this.recentItemSalesInfo[gameAppID][marketHashName] = recentItemSalesInfo;
+        }
+    }
+
     isAllItemPricesRetrieved(gameAppID) {
         return this.allItemPrices[gameAppID] !== undefined;
     }
 
     isPriceDataForItemsOnSaleRetrieved(gameAppID) {
         return this.priceDataForItemsOnSale[gameAppID] !== undefined;
+    }
+
+    isRecentItemSalesInfoRetrieved(marketHashName, gameAppID) {
+        if (!this.recentItemSalesInfo[gameAppID]) { return false; }
+        return this.recentItemSalesInfo[gameAppID][marketHashName] !== undefined;
     }
 
     async getAllItemPrices(appid) {
@@ -70,5 +83,22 @@ export default class AssetPrice {
             }
         }
         return this.priceDataForItemsOnSale[appid];
+    }
+
+    
+    async getRecentSalesInfo(marketHashName, appid) {
+        let authToken = totp.totp.gen(base32.decode(`${BitSkinsSecret}`));
+        const versionName = '1';
+        const methodName = 'get_sales_info';
+        if (!this.recentItemSalesInfo[appid]) { this.recentItemSalesInfo[appid] = {} };
+        if (!this.recentItemSalesInfo[appid][marketHashName]) {
+            try {
+                const res = await axios.post(`https://bitskins.com/api/v${versionName}/${methodName}/?api_key=${BitSkinsAPIKey}&market_hash_name=${marketHashName}&page=1&app_id=${appid}&code=${authToken}`);
+                return res.data.data.sales;
+            } catch (error) {
+                console.log(error);
+            }
+        } 
+        return this.recentItemSalesInfo[appid][marketHashName];
     }
 }
